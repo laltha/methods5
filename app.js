ee1ff593037c21a44a033a88d2a5e729f3591c73
laltha <156647324+laltha@users.noreply.github.com>
2024-02-13 16:47:48 +0530

    Update app.js
    changed login success!

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.post('/login/', async (request, response) => {
       databaseUser.password,
     )
     if (isPasswordMatched === true) {
-      response.send('Login success')
+      response.send('Login success!')
     } else {
       response.status(400)
       response.send('Invalid password')
